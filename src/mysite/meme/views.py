1d05a77aad90b3111af9f7611e5151513948a0dd
Yixin Zheng <yixinzh@umich.edu>
2021-04-21 10:46:08 -0400

    add text and download image

--- src/mysite/meme/views.py
+++ src/mysite/meme/views.py
@@ -70,47 +70,94 @@ def flickr(request):
 
 	return f
 
-def getPhotosets(flickr):
-	
-	photoset_list = flickr.photosets.getList(user_id='73509078@N00',per_page='5') \
-		.find('photosets') \
-		.findall('photoset')
-	photoset_list_array = []
-	for photoset in photoset_list:
-		photoset_id = photoset.attrib['id']
-		photoset_title = photoset.find('title').text
-		photoset_list_array.append({
-			'id':photoset_id,
-			'name':photoset_title})
-	return photoset_list_array
-
-def getPhotos(flickr,id):
-	photoset_photos = flickr.photosets.getPhotos(photoset_id=id,per_page='5') \
-		.find('photoset').findall('photo')
-	photoset_photos_list = []
-	for photo in photoset_photos:
-		photo_id = photo.attrib['id']
+def getPhotoSearch(request,flickr):
+	strval = request.GET.get("search",None)
+	# if strval:
+	photo_list = flickr.photos.search(tags=[{strval}],per_page='8') \
+		.find('photos') \
+		.findall('photo')
+	photos= []
+	for photo in photo_list:
+		photo_id=photo.attrib['id']
+		owner = photo.attrib['owner']
 		secret = photo.attrib['secret']
-		farm = photo.attrib['farm']
 		server = photo.attrib['server']
 		title = photo.attrib['title']
 		photo_url = \
-		'http://farm%s.static.flickr.com/%s/%s_%s.jpg'  \
-			% (farm,server,photo_id,secret)
-		photoset_photos_list.append(photo_url)
-	return photoset_photos_list
+			f'https://live.staticflickr.com/{server}/{photo_id}_{secret}.jpg'
+		photos.append({'id':photo_id,'url':photo_url})
+	return photos
+
+
+
+# def getPhotosets(flickr):
+	
+# 	photoset_list = flickr.photosets.getList(user_id='73509078@N00',per_page='5') \
+# 		.find('photosets') \
+# 		.findall('photoset')
+
+# 	photo_url_list=[]
+# 	for photo in photos:
+# 		photo_id=photo.attrib['id']
+# 		owner = photo.attrib['owner']
+# 		secret = photo.attrib['secret']
+# 		server = photo.attrib['server']
+# 		title = photo.attrib['title']
+# 		photo_url = \
+# 			f'https://live.staticflickr.com/{server}/{photo_id}_{secret}.jpg'
+# 		photo_url_list.append(photo_url)
+# 	return photo_url_list
+
+
+# def getPhotos(flickr,id):
+# 	photoset_photos = flickr.photosets.getPhotos(photoset_id=id,per_page='5') \
+# 		.find('photoset').findall('photo')
+# 	photoset_photos_list = []
+# 	for photo in photoset_photos:
+# 		photo_id = photo.attrib['id']
+# 		secret = photo.attrib['secret']
+# 		farm = photo.attrib['farm']
+# 		server = photo.attrib['server']
+# 		title = photo.attrib['title']
+# 		photo_url = \
+# 		'http://farm%s.static.flickr.com/%s/%s_%s.jpg'  \
+# 			% (farm,server,photo_id,secret)
+# 		photoset_photos_list.append(photo_url)
+# 	return photoset_photos_list
+
+
+# def getPhotoUrls(photos):
+# 	photo_url_list=[]
+# 	for photo in photos:
+# 		photo_id=photo.attrib['id']
+# 		owner = photo.attrib['owner']
+# 		secret = photo.attrib['secret']
+# 		server = photo.attrib['server']
+# 		title = photo.attrib['title']
+# 		photo_url = \
+# 			f'https://live.staticflickr.com/{server}/{photo_id}_{secret}.jpg'
+# 		photo_url_list.append(photo_url)
+# 	return photo_url_list
 
 
 @require_flickr_auth
 def index(request):
 	f = flickr(request)
-	photosets = getPhotosets(f)
-	output = []
-	for photoset in photosets:
-		photos = getPhotos(f,photoset['id'])
-		output.append({
-			'album_name':photoset['name'],
-			'photo_list': photos})
+	# photosets = getPhotosets(f)
+	photos = getPhotoSearch(request,f)
+	
+	output = photos
+	# output = [{'photo_url':p} for p in photo_list]
+
+	# for url in photo_url_list:
+	# 	output.append({'url':url})
+
+
+	# for photo in photos:
+	# 	photo = getPhotos(f,photo['id'])
+	# 	output.append({
+	# 		'album_name':photo['name'],
+	# 		'photo_list': photo})
 
 	context = {'output':output}
 	return render(request,
